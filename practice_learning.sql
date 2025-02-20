b16d6d2c42988f034e94bf460ea158cc2259d962
Sai Eshwar Reddy <nanieaswar@gmail.com>
2025-02-20 05:00:53 -0600

    sql review practice

---
+++ practice_learning.sql
@@ -0,0 +1,76 @@
+create database student;
+
+use student;
+create table student (
+	id int primary key,
+	name varchar(50),
+	age int not null
+);
+
+insert into student values(1,"sai",22);
+insert into student values(2,"pavan",22);
+
+select * from student;
+ 
+ show databases;
+ show tables;
+ 
+ 
+ drop table if exists student;
+ 
+ 
+ create table student(
+	rollno int primary key,
+    name varchar(40)
+    );
+    
+insert into student
+(rollno,name) 
+values
+(1,"sai"),
+(2,"pavan"),
+(3,"kiran");
+
+
+insert into student values (4,"ram");
+
+select * from student;
+
+###############################
+# PQ-1:
+
+create database xyz_company;
+use xyz_company;
+
+create table employee(
+	id int primary key,
+	name varchar(50),
+    salary int 
+);
+
+
+insert into employee
+(id,name,salary)
+values
+(1,"adam",25000),
+(2,"bob",30000),
+(3,"casey",40000);
+
+
+select * from employee;
+
+#####################################
+
+#PQ-2
+
+
+
+
+
+
+
+
+
+
+ 
+ 
